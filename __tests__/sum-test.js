435be0bf8ec497f4085765c35c8cc0226ab5a16e
FlemmingBehrend <feb@topdanmark.dk>
2018-06-07 10:01:10 +0200

    added console.log example

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -1,6 +1,12 @@
 describe('sum', function() {
-  it('adds 1 + 2 to equal 3', function() {
-    var sum = require('../sum');
-    expect(sum(1, 2)).toBe(3);
-  });
+    it('adds 1 + 2 to equal 3', function() {
+        var sum = require('../sum');
+        expect(sum(1, 2)).toBe(3);
+    });
+    it('ensures that log was called 2 times', () => {
+        global.console.log = jest.fn();
+        var sum = require('../sum');
+        sum(1, 2);
+        expect(console.log.mock.calls.length).toBe(2) //?
+    })
 });
--- sum.js
+++ sum.js
@@ -1,6 +1,8 @@
 
 function sum(value1, value2) {
-  return value1 + value2;
+    console.log('first log statement');
+    console.log('second log statement');
+    return value1 + value2;
 }
 
 module.exports = sum;
